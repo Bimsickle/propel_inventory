0606532b83b24d615bbac32e591aa74095eb3253
sucide-music <vi.koluzaeva@gmail.com>
2021-09-26 23:50:54 +0100

    chore (Homepage): added react components

--- src/components/StartingScreen.js
+++ src/components/StartingScreen.js
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function StartingScreen() {
-  return <div className="StartingScreen">Hello from StartingScreen</div>;
+  return (
+    <div className="StartingScreen">
+      <div>Greeting</div>
+      <a>Log in</a>
+      <a>Sign up</a>
+    </div>
+  );
 }
