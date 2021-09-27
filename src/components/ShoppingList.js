4a3dcdf99322689914c2fef5933663984991d4ea
sucide-music <vi.koluzaeva@gmail.com>
2021-09-27 23:39:19 +0100

    feature (Shopping-list): added preliminary structure and initial functions

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,5 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function ShoppingList() {
-  return <div className="ShoppingList">Hello from ShoppingList</div>;
+  let [shoppingItem, setShoppingItem] = useState(null);
+  function addShoppingItem(event) {
+    event.preventDefault();
+    alert(`${shoppingItem} added to your Shopping List!`);
+  }
+
+  function handleShoppingItemChange(event) {
+    event.preventDefault();
+    setShoppingItem(event.target.value);
+  }
+  return (
+    <div className="ShoppingList">
+      <ul>
+        <li>Fish fingers</li>
+        <li>Sausages</li>
+        <li>Smoked salmon</li>
+        <li>Prawns</li>
+      </ul>
+      <form onSubmit={addShoppingItem}>
+        <input
+          type="search"
+          id="search-form"
+          onChange={handleShoppingItemChange}
+        />
+        <input
+          type="submit"
+          value="Add item to shopping list"
+          className="btn btn-primary"
+        />
+      </form>
+    </div>
+  );
 }
