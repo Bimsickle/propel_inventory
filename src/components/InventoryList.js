c422ee521948410b94f04c164efa576a6ccc9f50
sucide-music <vi.koluzaeva@gmail.com>
2021-09-28 23:03:51 +0100

    chore (Inventory-list): added an array and mapping function instead of hardcoded data

--- src/components/InventoryList.js
+++ src/components/InventoryList.js
@@ -5,10 +5,23 @@ import ItemList from "./ItemList";
 export default function InventoryList() {
   let [loaded, setLoaded] = useState(false);
   let [item, setItem] = useState("null");
+  let products = [
+    { name: "Apples", expire: "12/05", quantity: 1, location: "fridge" },
+    { name: "Bananas", expire: "02/05", quantity: 3, location: "cabinet" },
+    { name: "Oranges", expire: "03/03", quantity: 10, location: "cabinet" },
+    { name: "Grapes", expire: "04/02", quantity: 4, location: "fridge" },
+    { name: "Melon", expire: "23/06", quantity: 5, location: "fridge" },
+    { name: "Berries", expire: "23/04", quantity: 2, location: "fridge" },
+  ];
+
+  function sortExpiry(event) {
+    event.preventDefault();
+    alert("Hello from sortExpiry");
+  }
 
   function addItem(event) {
     event.preventDefault();
-    alert(`${item} added`);
+    alert(`${item} added to inventory!`);
   }
 
   function handleItemChange(event) {
@@ -18,14 +31,29 @@ export default function InventoryList() {
 
   return (
     <div className="InventoryList">
-      <ul>
-        <li>Apples</li>
-        <li>Bananas</li>
-        <li>Oranges</li>
-        <li>Grapes</li>
-        <li>Melon</li>
-        <li>Berries</li>
-      </ul>
+      <table>
+        <caption>Inventory List</caption>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Expiry Date</th>
+            <th>Quantity</th>
+            <th>Location</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map(function (product, index) {
+            return (
+              <tr key={index}>
+                <td>{product.name}</td>
+                <td>{product.expire}</td>
+                <td>{product.quantity}</td>
+                <td> {product.location}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
       <form onSubmit={addItem}>
         <input type="search" id="search-form" onChange={handleItemChange} />
         <input
@@ -36,7 +64,7 @@ export default function InventoryList() {
       </form>
 
       <div>Sort inventory by:</div>
-      <button>Expiry date</button>
+      <button onClick={sortExpiry}>Expiry date</button>
       <button>Items low in stock</button>
       <button>Items by location</button>
     </div>
