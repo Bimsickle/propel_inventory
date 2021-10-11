3ebbe449ac9c6d09cb4ab5d7d3ff54639b9d3772
sucide-music <vi.koluzaeva@gmail.com>
2021-10-11 22:20:41 +0100

    Chore (Alerts): added Alerts layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Homepage from "./components/Homepage";
-import StartingScreen from "./components/StartingScreen";
 import { BrowserRouter, Route, Link } from "react-router-dom";
 import HomepageNav from "./components/HomepageNav";
 import Header from "./components/Header";
 import ItemDetails from "./components/ItemDetails";
 import AddItemOverlay from "./components/AddItemOverlay";
 import AddItemManually from "./components/AddItemManually";
+import Alerts from "./components/Alerts";
 
 function App() {
   return (
