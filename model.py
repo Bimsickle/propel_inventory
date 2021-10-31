05748dceae80807901497773d50d7e9e353aed4f
Ng Wei Han <weihan774237@gmail.com>
2021-10-31 10:03:09 +0800

    refactor(item): Changed datetime to date

--- model.py
+++ model.py
@@ -1,22 +1,26 @@
 from typing import Optional,List
 from datetime import datetime, date
-from pydantic import BaseModel
+from pydantic import BaseModel,validator
 
 class Item(BaseModel):
-    name: str
+    name: Optional[str] = None
     code: Optional[str] = None
     description: Optional[str] = None
     size: Optional[str] = None
     ingredients: Optional[List] = None
     allergy_info: Optional[str] = None
 class Inventory(BaseModel):
-    item: Item
-    quantity: int 
-    exp_date: Optional[datetime] = None
+    item: Optional[Item] = None
+    quantity: Optional[int] = None
+    exp_date: Optional[date] = None
     location: Optional[str] = None
 
+    @validator('exp_date',pre=True)
+    def parse_date(cls,value):
+        return datetime.strptime(value, "%m-%d-%Y").date()
+
 class ShoppingList(BaseModel):
-    date: Optional[datetime] = None
-    item: Item
-    quantity: int
+    date: Optional[date] = None
+    item: Optional[Item] = None
+    quantity: Optional[int] = None
     bought: Optional[bool] = False
